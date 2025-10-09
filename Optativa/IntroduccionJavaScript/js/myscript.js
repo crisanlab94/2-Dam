da8f8a518600477facd5986188b9c264ce4b18f9
crisanlab94 <cristinasandoval13@gmail.com>
2025-10-09 14:08:04 +0200

    cambios

--- Optativa/IntroduccionJavaScript/js/myscript.js
+++ Optativa/IntroduccionJavaScript/js/myscript.js
@@ -190,20 +190,39 @@ $("#btn").addEventListener("click", function () {
     var input = document.createElement("input");
     input.setAttribute("type", "email");
     input.setAttribute("placeholder", "E-mail");
-    input.setAttribute("name", "emails [] ");
+    input.setAttribute("name", "emails[]")
     $("#form").appendChild(input);
     myAlert("Add new email input")
-
 });
 
 function myAlert(msg) {
+    console.log($("body").children[1]);
     var div = document.createElement("div");
     div.classList.add("alert");
     div.innerHTML = msg;
-    $("body").insertBefore(div, $("body").firstChild)
+    var close = document.createElement("span");
+    close.style.float = "right";
+    close.classList.add("close");
+    close.innerHTML = "X";
+    div.appendChild(close);
+    $("body").insertBefore(div, $("body").firstChild);
+    bind_close();
+
+}
+
+function bind_close() {
+    let elements = document.querySelectorAll(".close");
+    for (var i = elements.length - 1; i >= 0; i--) {
+        let el = elements[i];
+        el.addEventListener("click", function () {
+            this.parentNode.style.display = "none";
+        });
+    }
 }
 
 function $(selector) {
     return document.querySelector(selector);
 }
+
+
 
