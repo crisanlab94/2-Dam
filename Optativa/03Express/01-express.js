2849040991853fe0505de58ea0bc68142cf7f81e
crisanlab94 <cristinasandoval13@gmail.com>
2025-11-10 09:04:38 +0100

    optativa

---
+++ Optativa/03Express/01-express.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname +'/public'));
+
+app.get('/', (req, res) => {
+  //console.log(__dirname) //ruta donde estamos
+  res.send('Ya somos unos crack en Node+Express')
+})
+
+app.get('/contacto', (req, res) => {
+  console.log(__dirname + '/public')
+  res.send('Estas en contactos')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
